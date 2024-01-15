6acb1cfe0e162769940f91f576d566436e6b02b3
AndreaBasile97 <andreabasile97@gmail.com>
2024-01-15 10:39:24 +0100

    Fix make_datasets

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,60 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from keras.utils import to_categorical
+import gzip
+import numpy as np
+import os
+from keras.datasets import mnist
+
+# Adjust the paths to your actual file locations
+train_images_path = "data/raw/train-images.gz"
+train_labels_path = "data/raw/train-labels.gz"
+test_images_path = "data/raw/test-images.gz"
+test_labels_path = "data/raw/test-labels.gz"
+
+interim_folder = "data/interim"
+processed_folder = "data/processed"
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def load_mnist_data():
+    (trainX, trainY), (testX, testY) = mnist.load_data()
+    print(trainX.shape)
+    print(testX.shape)
+    return (trainX, trainY), (testX, testY)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+def intermediate_process(trainX, testX, trainY, testY):
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    # Save artifacts
+    np.save(os.path.join(interim_folder, "trainX.npy"), trainX)
+    np.save(os.path.join(interim_folder, "testX.npy"), testX)
+    np.save(os.path.join(interim_folder, "trainY.npy"), trainY)
+    np.save(os.path.join(interim_folder, "testY.npy"), testY)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    return trainX, trainY, testX, testY
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
-    main()
+# scale pixels
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype("float32")
+    test_norm = test.astype("float32")
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # Save normalized images
+    np.save(os.path.join(processed_folder, "train_norm.npy"), train_norm)
+    np.save(os.path.join(processed_folder, "test_norm.npy"), test_norm)
+
+    return train_norm, test_norm
+
+
+(trainX, trainY), (testX, testY) = load_mnist_data(
+    train_images_path, train_labels_path, test_images_path, test_labels_path
+)
+
+trainX, testX, trainY, testY = intermediate_process(trainX, testX, trainY, testY)
+preprocessed_dataset = prep_pixels(trainX, testX)
